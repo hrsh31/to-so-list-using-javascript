f62386c63a4e098a3f016e67616745f4c699f830
harsh gupta <harshgupta3199@gmail.com>
2020-05-17 10:28:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+//localStorage.tasks=JSON.stringify([]);
+updateList();
+function addEvent(){
+	let input=document.getElementById("task_input");
+
+	let tasksArr=JSON.parse(localStorage.tasks);
+	tasksArr.push(input.value);
+	localStorage.tasks=JSON.stringify(tasksArr);
+	input.value="";
+	updateList();
+}
+function updateList(){
+	let ul=document.getElementById("tasks");
+
+	let tasksArr=JSON.parse(localStorage.tasks);
+	let length=tasksArr.length-1;
+	let html="";
+
+	for(let i=length;i>=0;i--){
+		html+='<li class="list-group-item" id="' + i + '">'+ tasksArr[i] +' <span class="btn btn-danger btn-sm float-right remove_btn" onclick="removeItem(this)">remove</span></li>';
+	}
+	ul.innerHTML=html;
+
+}
+function removeItem(span_tag){
+	let index_id=span_tag.parentElement.id;
+	let tasksArr=JSON.parse(localStorage.tasks);
+	tasksArr.splice(index_id,1);
+	localStorage.tasks=JSON.stringify(tasksArr);
+	updateList();
+}
